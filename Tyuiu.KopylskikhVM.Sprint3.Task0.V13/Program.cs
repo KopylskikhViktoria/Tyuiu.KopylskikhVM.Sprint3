93ff473797d9a090df736253abe26814102a2b5f
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-29 19:19:16 +0500

    Task0

--- Tyuiu.KopylskikhVM.Sprint3.Task0.V13/Program.cs
+++ Tyuiu.KopylskikhVM.Sprint3.Task0.V13/Program.cs
@@ -38,23 +38,6 @@ internal class Program
 
         Console.WriteLine(ds.GetSumSeries(zzz, startValue, stopValue));
 
-        int a = 2;
-        int b = 4;
-        int c = 3;
-        int d = 5;
-        int i = 1;
-
-        while (i < 4)
-        {
-            b++;
-            b -= a;
-            c = b - a;
-            d = (c + a) + i;
-            i++;
-        }
-
-        Console.WriteLine(d);
-
         Console.ReadLine();
     }
 }
